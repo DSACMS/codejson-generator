7491999bbc9eb690c737f5ac3d6eb447438469a1
Natalia Luzuriaga <natalia.luzuriaga@cms.hhs.gov>
2025-08-22 11:49:41 -0700

    Changed styling of part 2
    Signed-off-by: Natalia Luzuriaga <natalia.luzuriaga@cms.hhs.gov>

--- js/generateFormComponents.js
+++ js/generateFormComponents.js
@@ -256,7 +256,7 @@ function createFormHeading(title, description) {
 
 function createAutoGenerationBox() {
 	const container = document.getElementById("auto-generation-header")
-	container.innerHTML = `<div class="step-header"><div class="step-number">2</div><h3>Auto Generate Fields</h3></div> \n <h4> Please enter your repositories GitHub URL in order to automatically pre-fill some of the fields in this form! </h4> \n <h6> <i>This currently only works on <b>public</b> repositories</i> </h6>`
+	container.innerHTML = `<div class="step-header"><div class="step-number">2</div><h2>Auto Generate Fields</h2></div><h4 id="quiz-subheading"> Enter your repository's GitHub URL in order to automatically pre-fill some of the fields in this form </h4> \n <p> <i>This currently only works on <b>public</b> repositories</i> </p>`
 }
 
 // Iterates through each json field and creates component array for Form.io
